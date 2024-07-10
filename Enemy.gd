7b305e86fdfdb6db1abaa6fe74d0f5f2b24d605c
VintageBadger <sleepypandas2@gmail.com>
2024-07-10 13:43:13 -0500

    multiple targets with Obj name not Coords

--- Enemy.gd
+++ Enemy.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 #@export var target : Node2D
-#var targets = [$"../Target1", $"../Target2"]
-var targets = [Vector2(212, 77), Vector2(209, 184)]
+@onready var targets = [$"../Target1", $"../Target2"]
+#var targets = [Vector2(212, 77), Vector2(209, 184)]
 var target_index = 0
-var target = targets[target_index]
+@onready var target = targets[target_index]
 var end_pos = Vector2(61, 78)
 
 var speed = 100
@@ -13,7 +13,6 @@ var acceleration = 7
 @onready var navigation_agent : NavigationAgent2D = $Navigation/NavigationAgent2D
 
 func _physics_process(delta):
-	#target = get_node(targets[target_index])
 	var direction = Vector2.ZERO
 	direction = navigation_agent.get_next_path_position() - global_position
 	direction = direction.normalized() #since we dont care about magnitude 
@@ -23,13 +22,15 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func _on_timer_timeout():
-	#target = get_node(targets[target_index])
-	#target = targets[target_index]
 	if target == null:
 		return
-	navigation_agent.target_position = target #if Vector2
-	#navigation_agent.target_position = target.global_position #this is how we tell navAgent where its goal is
-	
+	match typeof(target):
+		TYPE_VECTOR2:
+			navigation_agent.target_position = target #if Vector2
+		TYPE_OBJECT:
+			navigation_agent.target_position = target.global_position #this is how we tell navAgent where its goal is
+		_:
+			print("other type of target : ", typeof(target))
 
 
 func _on_navigation_agent_2d_target_reached():
@@ -39,5 +40,6 @@ func _on_navigation_agent_2d_target_reached():
 		#target_index = 0 #loop back to first 
 		target = end_pos #return to starting position
 	else:
-		target = targets[target_index]
+		#target = targets[target_index]
+		target = targets[target_index].global_position
 	navigation_agent.target_position = target
